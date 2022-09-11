5fe8cd00e060b18b22ab6965ace45f18b02a265e
Nazedoh <nazedoh@gmail.com>
2022-09-11 07:00:21 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - entry point
- * 
+ * main - Entry point
+ *
  * Return: 0 after printing the function
  */
 int main(void)
 {
-	puts(" \ "Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
