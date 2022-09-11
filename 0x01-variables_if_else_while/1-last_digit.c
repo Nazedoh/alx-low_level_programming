7138d5d5e0025fbe835094f0bd3c3038f9e84c29
Nazedoh <nazedoh@gmail.com>
2022-09-11 10:57:00 -0700

    task2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,34 @@
-
-
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * main - Entry point
+ *  * main - The last digit
+ *   * @void: Empty parameter list for main
  *
- * Return: Always 0 (Success)
+ *   Description: Prints last digit of random number and tells 
+ *   if it is greater than 5, 0, or less than 6 and not 0.
+ *
+ *   Return: 0 for success
  */
 int main(void)
 {
-int n, last_digit;
-
-srand(time(0));
+int n;
+srand(time(0));	
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10;
+printf("Last digit of %d is %d ", n, (n % 10));
 
-if (last_digit > 5)
-	printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
-else if (last_digit == 0)
-	printf("Last digit of %i is %i and is 0\n", n, last_digit);
-else if (last_digit < 6)
-	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
-	return (0);
+	if ((n % 10) > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+      return (0);
 }
+
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase, then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l_letter;
+	char u_letter;
+
+	for (l_letter = 97; l_letter <= 122; l_letter++
+			{
+	
