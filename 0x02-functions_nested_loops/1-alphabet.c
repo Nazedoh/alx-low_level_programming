3424f9686460e367d0124f96a9c80bb85abc2280
Nazedoh <nazedoh@gmail.com>
2022-09-14 02:14:27 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * *print_alphabet - the alphabet, in lowercase, followed by a new line.
- * * Return: Always 0 (Success)
- * *
- * */
+ * print_alphabet - the alphabet, in lowercase.
+ * Return: Always 0 (Success)
+ *
+ */
 
 
 void print_alphabet(void)
 
 {
 	int n;
+
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		_putchar(n);
