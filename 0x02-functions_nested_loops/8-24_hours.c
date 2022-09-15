df699d3499e9f48d09f788f377dabe292137021d
Nazedoh <nazedoh@gmail.com>
2022-09-14 18:27:57 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,18 +11,16 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i < 24)
+	for (i = 0; i < 23; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (j = 0; j < 59; j++)
 		{
 			_putchar((i / 10) + '0');
-
-			++i;
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');;
 		}
-
 	}
-
 }
-
